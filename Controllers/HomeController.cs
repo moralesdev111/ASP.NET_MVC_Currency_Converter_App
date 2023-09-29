917cd29e7546671ccd1489a59c8fb25bca6bb968
Guillermo Mora <moralesdev111@gmail.com>
2023-09-30 00:18:04 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+using System.Diagnostics;
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+            
+        }
+        [HttpGet]
+        public IActionResult CurrencyMenu(decimal _valueInput)
+        {
+            Currency currency = new Currency();
+            currency.valueInput = _valueInput;
+            return View(currency);            
+        }
+        [HttpPost]
+        public IActionResult CurrencyMenu(Currency currency)
+        {
+            if (ModelState.IsValid)
+            {
+                currency.valueOutput = currency.valueInput * 1.06M;
+                return View("Output",currency);
+            }
+            
+
+                return View("CurrencyMenu",currency);
+            }
+
+        }
+
+
+       
+      
+        
+
+
+        
+    }
---
+++ Models/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCProject.Models
+{
+    public class Currency
+    {
+        [Required]
+        [Display (Name = "Currency Amount in €")]
+        public decimal valueInput {  get; set; }
+        [Display (Name = "Resulting Currency in $")]
+        public decimal valueOutput { get; set; }
+
+     
+    }
+}
